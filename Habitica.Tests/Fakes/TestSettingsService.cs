9753d54a42897d86a669ba70742f18e0bd19647b
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-13 23:19:27 +0000

    fake settings service

---
+++ Habitica.Tests/Fakes/TestSettingsService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using TIKSN.Settings;
+
+namespace TIKSN.Habitica.Tests.Fakes
+{
+    public class TestSettingsService : ISettingsService
+    {
+        private readonly IConfigurationRoot _configurationRoot;
+
+        public TestSettingsService(IConfigurationRoot configurationRoot)
+        {
+            _configurationRoot = configurationRoot ?? throw new ArgumentNullException(nameof(configurationRoot));
+        }
+
+        public T GetLocalSetting<T>(string name, T defaultValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetRoamingSetting<T>(string name, T defaultValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<string> ListLocalSetting()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<string> ListRoamingSetting()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveLocalSetting(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRoamingSetting(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLocalSetting<T>(string name, T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetRoamingSetting<T>(string name, T value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

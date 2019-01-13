937508447dc515d666f48ed3cf4262e1f115bdf2
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-13 23:18:51 +0000

    client added

---
+++ Habitica/HabiticaClient.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Habitica
+{
+    public class HabiticaClient : IHabiticaClient
+    {
+        private readonly IRestClientFactory _restClientFactory;
+
+        public HabiticaClient(IRestClientFactory restClientFactory)
+        {
+            _restClientFactory = restClientFactory ?? throw new ArgumentNullException(nameof(restClientFactory));
+        }
+
+        public async Task GetUserProfileAsync(CancellationToken cancellationToken)
+        {
+            var request = new RestRequest("user", Method.GET);
+
+            var restClient = _restClientFactory.Create();
+
+            var response = await restClient.ExecuteTaskAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ Habitica/IHabiticaClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Habitica
+{
+    public interface IHabiticaClient
+    {
+        Task GetUserProfileAsync(CancellationToken cancellationToken);
+    }
+}

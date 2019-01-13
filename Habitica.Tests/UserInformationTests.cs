3def9f59d6c46d20e1867c35e6b7a2b0a3d817bb
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-13 23:19:58 +0000

    user information tests added

---
+++ Habitica.Tests/UserInformationTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using TIKSN.Habitica.Tests.Fixture;
+using Xunit;
+
+namespace TIKSN.Habitica.Tests
+{
+    [Collection("ServiceProvider")]
+    public class UserInformationTests
+    {
+        private readonly ServiceProviderFixture _serviceProviderFixture;
+
+        public UserInformationTests(ServiceProviderFixture serviceProviderFixture)
+        {
+            _serviceProviderFixture = serviceProviderFixture ?? throw new ArgumentNullException(nameof(serviceProviderFixture));
+        }
+
+        [Fact]
+        public async Task GetUserProfile()
+        {
+            var habiticaClient = _serviceProviderFixture.ServiceProvider.GetRequiredService<IHabiticaClient>();
+        }
+    }
+}

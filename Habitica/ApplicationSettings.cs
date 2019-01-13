4435bde62688994fce41fac9b2cc8658dd921e05
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-13 05:20:35 +0200

    read and write settings

---
+++ Habitica/ApplicationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TIKSN.Settings;
+
+namespace TIKSN.Habitica
+{
+    public class ApplicationSettings : IApplicationSettings
+    {
+        private readonly ISettingsService _settingsService;
+
+        public ApplicationSettings(ISettingsService settingsService)
+        {
+            _settingsService = settingsService ?? throw new ArgumentNullException(nameof(settingsService));
+        }
+
+        public string UserID { get => _settingsService.GetLocalSetting("habitica-user-id", string.Empty); set => _settingsService.SetLocalSetting("habitica-user-id", value); }
+        public string ApiKey { get => _settingsService.GetLocalSetting("habitica-api-key", string.Empty); set => _settingsService.SetLocalSetting("habitica-api-key", value); }
+    }
+}

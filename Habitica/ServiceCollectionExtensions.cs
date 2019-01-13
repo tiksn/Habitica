0417a35c89754554cba0fb473be6dc65d304bdee
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-13 05:27:31 +0200

    services registered

---
+++ Habitica/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TIKSN.Habitica
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddHabitica(this IServiceCollection services)
+        {
+            services.AddSingleton<IRestClientFactory, RestClientFactory>();
+            services.AddSingleton<IApplicationSettings, ApplicationSettings>();
+        }
+    }
+}

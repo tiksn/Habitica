a493579100fde5a9cd32a9154b1a0224759f9847
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-14 00:15:32 +0000

    data model added

---
+++ Habitica/Models/UltimateGearSets.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace TIKSN.Habitica.Models
+{
+    public class UltimateGearSets
+    {
+        [JsonProperty("healer")]
+        public bool Healer { get; set; }
+
+        [JsonProperty("wizard")]
+        public bool Wizard { get; set; }
+
+        [JsonProperty("rogue")]
+        public bool Rogue { get; set; }
+
+        [JsonProperty("warrior")]
+        public bool Warrior { get; set; }
+    }
+}

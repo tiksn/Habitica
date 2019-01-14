381c78095dc474759e64b796c51d38572e7fb0d5
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-14 00:15:41 +0000

    data model added

---
+++ Habitica/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace TIKSN.Habitica.Models
+{
+    public class UserModel
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        [JsonProperty("data")]
+        public UserData Data { get; set; }
+
+        [JsonProperty("notifications")]
+        public object[] Notifications { get; set; }
+
+        [JsonProperty("appVersion")]
+        public string AppVersion { get; set; }
+    }
+}

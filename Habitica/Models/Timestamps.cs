c68e021518f036f45e54354c14dc825cb024f66d
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-14 00:15:25 +0000

    data model added

---
+++ Habitica/Models/Timestamps.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace TIKSN.Habitica.Models
+{
+    public class Timestamps
+    {
+        [JsonProperty("created")]
+        public DateTimeOffset Created { get; set; }
+
+        [JsonProperty("loggedin")]
+        public DateTimeOffset Loggedin { get; set; }
+    }
+}

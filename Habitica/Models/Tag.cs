d75af479f063079bca36f6e3057368146f6b281b
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-14 00:15:17 +0000

    data model added

---
+++ Habitica/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace TIKSN.Habitica.Models
+{
+    public partial class Tag
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}

6827ecf02dc17d85554baf16512607d08c8fedf9
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-14 00:15:36 +0000

    data model added

---
+++ Habitica/Models/UserData.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace TIKSN.Habitica.Models
+{
+    public class UserData
+    {
+        [JsonProperty("auth")]
+        public Auth Auth { get; set; }
+
+        [JsonProperty("achievements")]
+        public Achievements Achievements { get; set; }
+
+        [JsonProperty("profile")]
+        public Profile Profile { get; set; }
+
+        [JsonProperty("stats")]
+        public Stats Stats { get; set; }
+
+        [JsonProperty("_id")]
+        public Guid Id { get; set; }
+
+        //[JsonProperty("notifications")]
+        //public object[] Notifications { get; set; }
+
+        [JsonProperty("id")]
+        public Guid DataId { get; set; }
+    }
+}

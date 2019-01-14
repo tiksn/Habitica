85e3d5500f5093640037ebb3ce26a20cd2d6436a
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-14 00:15:06 +0000

    data model added

---
+++ Habitica/Models/Local.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace TIKSN.Habitica.Models
+{
+    public class Local
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("lowerCaseUsername")]
+        public string LowerCaseUsername { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+    }
+}

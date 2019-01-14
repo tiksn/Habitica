309eeee58fa3d4ce12887505b165be8a0026caf2
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-14 00:15:21 +0000

    data model added

---
+++ Habitica/Models/TasksOrder.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace TIKSN.Habitica.Models
+{
+    public class TasksOrder
+    {
+        [JsonProperty("habits")]
+        public object[] Habits { get; set; }
+
+        [JsonProperty("dailys")]
+        public object[] Dailys { get; set; }
+
+        [JsonProperty("todos")]
+        public object[] Todos { get; set; }
+
+        [JsonProperty("rewards")]
+        public object[] Rewards { get; set; }
+    }
+}

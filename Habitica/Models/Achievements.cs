bc52e674b2ec25f962d6147cbc3acda5a05a072b
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-14 00:14:47 +0000

    data model added

---
+++ Habitica/Models/Achievements.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace TIKSN.Habitica.Models
+{
+    public class Achievements
+    {
+        [JsonProperty("ultimateGearSets")]
+        public UltimateGearSets UltimateGearSets { get; set; }
+
+        [JsonProperty("challenges")]
+        public object[] Challenges { get; set; }
+
+        [JsonProperty("perfect")]
+        public long Perfect { get; set; }
+
+        //[JsonProperty("quests")]
+        //public Quests Quests { get; set; }
+
+        [JsonProperty("partyUp")]
+        public bool PartyUp { get; set; }
+
+        [JsonProperty("partyOn")]
+        public bool PartyOn { get; set; }
+
+        [JsonProperty("habiticaDays")]
+        public long HabiticaDays { get; set; }
+    }
+}

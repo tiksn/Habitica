1871b66572a3a0e5d356259ee64119f501642bac
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-14 00:15:02 +0000

    data model added

---
+++ Habitica/Models/Facebook.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace TIKSN.Habitica.Models
+{
+    public class Facebook
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("emails")]
+        public string[] Emails { get; set; }
+    }
+}

968e02f5b58ac3ece3845037caf9d3055420973c
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-14 00:15:28 +0000

    data model added

---
+++ Habitica/Models/Training.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace TIKSN.Habitica.Models
+{
+    public class Training
+    {
+        [JsonProperty("int")]
+        public long Int { get; set; }
+
+        [JsonProperty("per")]
+        public long Per { get; set; }
+
+        [JsonProperty("str")]
+        public long Str { get; set; }
+
+        [JsonProperty("con")]
+        public long Con { get; set; }
+    }
+}

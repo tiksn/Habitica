0cdd7ea36ce246fc705873b2a50e8469dd68c635
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-20 23:56:36 +0000

    CreateOrGetTag method created

---
+++ Habitica/EisenhowerMatrix/TagInitializer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Habitica.Models;
+using TIKSN.Habitica.Rest;
+
+namespace TIKSN.Habitica.EisenhowerMatrix
+{
+    public class TagInitializer
+    {
+        private readonly IApplicationSettings _applicationSettings;
+        private readonly IHabiticaClient _habiticaClient;
+
+        public TagInitializer(IHabiticaClient habiticaClient, IApplicationSettings applicationSettings)
+        {
+            _habiticaClient = habiticaClient ?? throw new ArgumentNullException(nameof(habiticaClient));
+            _applicationSettings = applicationSettings ?? throw new ArgumentNullException(nameof(applicationSettings));
+        }
+
+        public async Task InitializeDefaultsAsync(CancellationToken cancellationToken)
+        {
+        }
+
+        private async Task<(TagsModel, TagData)> CreateOrGetTag(TagsModel tags, string name, CancellationToken cancellationToken)
+        {
+            var foundTag = tags.Data.FirstOrDefault(t => string.Equals(t.Name, name, StringComparison.OrdinalIgnoreCase));
+
+            if (foundTag != null)
+            {
+                return (tags, foundTag);
+            }
+
+            var tagsAfterCreation = await _habiticaClient.CreateTagAsync(name, cancellationToken);
+
+            var tag = tagsAfterCreation.Data.First(t => string.Equals(t.Name, name, StringComparison.OrdinalIgnoreCase));
+
+            return (tagsAfterCreation, tag);
+        }
+    }
+}

240203e7b87db69dc97f35546f12177b443ca0a4
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-21 05:29:41 +0000

    test

---
+++ Habitica.Tests/Rest/UserTasksTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using TIKSN.Habitica.Rest;
+using TIKSN.Habitica.Tests.Fixture;
+using Xunit;
+
+namespace TIKSN.Habitica.Tests.Rest
+{
+    [Collection("ServiceProvider")]
+    public class UserTasksTests
+    {
+        private readonly ServiceProviderFixture _serviceProviderFixture;
+
+        public UserTasksTests(ServiceProviderFixture serviceProviderFixture)
+        {
+            _serviceProviderFixture = serviceProviderFixture ?? throw new ArgumentNullException(nameof(serviceProviderFixture));
+        }
+
+        [Fact]
+        public async Task GetUserTasks()
+        {
+            var habiticaClient = _serviceProviderFixture.ServiceProvider.GetRequiredService<IHabiticaClient>();
+
+            var allUserTasks = await habiticaClient.GetUserTasksAsync(default);
+
+            allUserTasks.Data.Should().NotBeEmpty();
+        }
+    }
+}

28998ff0f3ec2735386038ad884458b03d1ae2c4
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-21 06:13:34 +0000

    matrix service added

---
+++ Habitica/EisenhowerMatrix/MatrixAndBacklogService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Habitica.EisenhowerMatrix.Models;
+using TIKSN.Habitica.Models;
+using TIKSN.Habitica.Rest;
+
+namespace TIKSN.Habitica.EisenhowerMatrix
+{
+    public class MatrixAndBacklogService : IMatrixAndBacklogService
+    {
+        private readonly IApplicationSettings _applicationSettings;
+        private readonly IHabiticaClient _habiticaClient;
+
+        public MatrixAndBacklogService(IHabiticaClient habiticaClient, IApplicationSettings applicationSettings)
+        {
+            _habiticaClient = habiticaClient ?? throw new ArgumentNullException(nameof(habiticaClient));
+            _applicationSettings = applicationSettings ?? throw new ArgumentNullException(nameof(applicationSettings));
+        }
+
+        public async Task<MatrixAndBacklog> GetMatrixAndBacklogAsync(CancellationToken cancellationToken)
+        {
+            // LINQ is not used for performance
+            var result = new MatrixAndBacklog();
+            var todos = await _habiticaClient.GetUserToDosAsync(cancellationToken);
+
+            foreach (var todo in todos.Data)
+            {
+                var isImportant = todo.Tags.Contains(_applicationSettings.ImportantTag);
+                var isUrgent = todo.Tags.Contains(_applicationSettings.UrgentTag);
+                var isLessImportant = todo.Tags.Contains(_applicationSettings.LessImportantTag);
+                var isLessUrgent = todo.Tags.Contains(_applicationSettings.LessUrgentTag);
+
+                AddToResult(result, todo, isImportant, isUrgent, isLessImportant, isLessUrgent);
+            }
+
+            return result;
+        }
+
+        private void AddToResult(MatrixAndBacklog result, TaskData todo, bool isImportant, bool isUrgent, bool isLessImportant, bool isLessUrgent)
+        {
+            // because of transient network failures, it is possible that only one of necessary tags are set
+            // that kind of to-dos should remain in backlog
+
+            var noAnomalies = (isImportant != isLessImportant) && (isUrgent != isLessUrgent);
+
+            if (isImportant && isUrgent && noAnomalies)
+                result.Matrix.DoFirstQuadrant.Add(todo);
+            else if (isImportant && isLessUrgent && noAnomalies)
+                result.Matrix.DelegateQuadrant.Add(todo);
+            else if (isLessImportant && isUrgent && noAnomalies)
+                result.Matrix.DelegateQuadrant.Add(todo);
+            else if (isLessImportant && isLessUrgent && noAnomalies)
+                result.Matrix.DoNotDoQuadrant.Add(todo);
+            else
+                result.Backlog.Add(todo);
+        }
+    }
+}

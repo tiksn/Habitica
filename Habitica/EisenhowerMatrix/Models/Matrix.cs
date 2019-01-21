099afa3127473299d3ae865edc33ba6a280f42b8
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-21 05:47:48 +0000

    models added

---
+++ Habitica/EisenhowerMatrix/Models/Matrix.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TIKSN.Habitica.Models;
+
+namespace TIKSN.Habitica.EisenhowerMatrix.Models
+{
+    public class Matrix
+    {
+        public List<TaskData> DelegateQuadrant { get; set; }
+
+        public List<TaskData> DoFirstQuadrant { get; set; }
+
+        public List<TaskData> DoNotDoQuadrant { get; set; }
+
+        public List<TaskData> ScheduleQuadrant { get; set; }
+    }
+}
---
+++ Habitica/EisenhowerMatrix/Models/MatrixAndBackLog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TIKSN.Habitica.Models;
+
+namespace TIKSN.Habitica.EisenhowerMatrix.Models
+{
+    public class MatrixAndBacklog
+    {
+        public List<TaskData> Backlog { get; set; }
+
+        public Matrix Matrix { get; set; }
+    }
+}

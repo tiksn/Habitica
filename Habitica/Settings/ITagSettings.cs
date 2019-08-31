2789ec10f9e8190bea7b5672cf51834f447b9b9f
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-31 15:50:09 +0300

    Create ITagSettings.cs

---
+++ Habitica/Settings/ITagSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TIKSN.Habitica.Settings
+{
+    public interface ITagSettings
+    {
+        bool HasImportantTag { get; }
+        bool HasLessImportantTag { get; }
+        bool HasLessUrgentTag { get; }
+        bool HasUrgentTag { get; }
+        Guid ImportantTag { get; set; }
+        Guid LessImportantTag { get; set; }
+        Guid LessUrgentTag { get; set; }
+        Guid UrgentTag { get; set; }
+    }
+}

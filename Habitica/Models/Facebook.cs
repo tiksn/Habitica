90d68c235dd1906476c8b42728423cf42467b58c
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-10-13 21:47:34 +0300

    Update Facebook.cs

--- Habitica/Models/Facebook.cs
+++ Habitica/Models/Facebook.cs
@@ -1,11 +1,12 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace TIKSN.Habitica.Models
 {
     public class Facebook
     {
         [JsonProperty("emails")]
-        public string[] Emails { get; set; }
+        public List<string> Emails { get; set; }
 
         [JsonProperty("id")]
         public string Id { get; set; }
